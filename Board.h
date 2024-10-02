ef4a131d4751084ff1cbea5eef972a10672ec558
LazarenkoDmytro <lazarenkodmytro.work@gmail.com>
2024-10-02 21:24:22 +0300

    Shape structure (Board needed, so it's added)

---
+++ Board.h
@@ -0,0 +1,10 @@
+//
+// Created by Dmytro Lazarenko on 02.10.2024.
+//
+
+#pragma once
+
+
+class Board {
+
+};
---
+++ Shape.h
@@ -0,0 +1,32 @@
+//
+// Created by Dmytro Lazarenko on 02.10.2024.
+//
+
+#pragma once
+
+
+#include <memory>
+#include <iostream>
+#include <sstream>
+#include "Board.h"
+
+class Shape {
+protected:
+    int id = 1;
+public:
+    Shape();
+
+    virtual ~Shape();
+
+    [[nodiscard]] virtual int getId() const;
+
+    virtual void draw(Board &board) const = 0;
+
+    [[nodiscard]] virtual bool isEqual(std::shared_ptr<Shape> other) const = 0;
+
+    [[nodiscard]] virtual std::string getInfo() const = 0;
+
+    virtual void save(std::ostream &out) const = 0;
+
+    static std::shared_ptr<Shape> load(std::istream &in);
+};
