d234294ff0d05849577301990900db9adf86854b
LazarenkoDmytro <lazarenkodmytro.work@gmail.com>
2024-10-02 22:35:45 +0300

    CLI structure (Application needed, so it is added)

---
+++ CLI.h
@@ -0,0 +1,17 @@
+//
+// Created by Dmytro Lazarenko on 02.10.2024.
+//
+
+#pragma once
+
+
+#include "Application.h"
+
+class CLI {
+private:
+    Application app;
+public:
+    CLI();
+
+    void run();
+};
