5b92e7e0a30636439c66446722749a9414f1e98b
LazarenkoDmytro <lazarenkodmytro.work@gmail.com>
2024-10-02 21:59:30 +0300

    Rectangle implementation

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include "Circle.h"
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+
+    Circle circle(1, 2, 3);
+    std::cout << circle.getID() << std::endl;
+
+    Circle circle2(1, 2, 3);
+    std::cout << circle2.getID() << std::endl;
+
     return 0;
 }
