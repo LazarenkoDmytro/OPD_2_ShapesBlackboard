ee9273d37527fab7bf355a5334489f54f80452f2
LazarenkoDmytro <lazarenkodmytro.work@gmail.com>
2024-10-02 21:40:18 +0300

    Circle structure

--- Circle.h
+++ Circle.h
@@ -8,5 +8,22 @@
 #include "Shape.h"
 
 class Circle : public Shape {
+private:
+    int x;
+    int y;
+    int radius;
+public:
+    Circle(int x, int y, int radius);
 
+    [[nodiscard]] int getID() const override;
+
+    void draw(Board &board) const override;
+
+    [[nodiscard]] bool isEqual(std::shared_ptr<Shape> other) const override;
+
+    [[nodiscard]] std::string getInfo() const override;
+
+    void save(std::ostream &out) const override;
+
+    static std::shared_ptr<Shape> load(std::istream &in);
 };
