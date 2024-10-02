934b025688325ca6280f831422c728731e818f22
LazarenkoDmytro <lazarenkodmytro.work@gmail.com>
2024-10-02 22:02:13 +0300

    Triangle structure

--- Triangle.h
+++ Triangle.h
@@ -8,5 +8,20 @@
 #include "Shape.h"
 
 class Triangle : public Shape {
+private:
+    int x1, y1, x2, y2, x3, y3;
+public:
+    Triangle(int x1, int y1, int x2, int y2, int x3, int y3);
 
+    [[nodiscard]] int getID() const override;
+
+    void draw(Board &board) const override;
+
+    [[nodiscard]] bool isEqual(std::shared_ptr<Shape> other) const override;
+
+    [[nodiscard]] std::string getInfo() const override;
+
+    void save(std::ostream &out) const override;
+
+    static std::shared_ptr<Shape> load(std::istream &in);
 };
