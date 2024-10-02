008569266063746d314a8f9208d0507d1ab3d51a
LazarenkoDmytro <lazarenkodmytro.work@gmail.com>
2024-10-02 21:52:11 +0300

    Rectangle structure

--- Rectangle.h
+++ Rectangle.h
@@ -8,5 +8,23 @@
 #include "Shape.h"
 
 class Rectangle : public Shape {
+private:
+    int x;
+    int y;
+    int width;
+    int height;
+public:
+    Rectangle(int x, int y, int width, int height);
 
+    [[nodiscard]] int getID() const override;
+
+    void draw(Board &board) const override;
+
+    [[nodiscard]] bool isEqual(std::shared_ptr<Shape> other) const override;
+
+    [[nodiscard]] std::string getInfo() const override;
+
+    void save(std::ostream &out) const override;
+
+    static std::shared_ptr<Shape> load(std::istream &in);
 };
